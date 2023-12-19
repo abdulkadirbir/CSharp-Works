5fabf50b77aa33d408c5664b9c51ef5dfbaf799b
Abdulkadir Bir <77737235+abdulkadirbir@users.noreply.github.com>
2023-12-19 23:22:42 +0300

    Add files via upload

---
+++ 1-console-programlama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace console_programlama;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        // System.Console.WriteLine("Hello, World!"); // System eğer yukarıda import edilmeseydi bu şekilde kullanılabilirdi.
+        Console.WriteLine("İsim giriniz : ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Soyisim giriniz : ");
+        string surname = Console.ReadLine();
+        Console.WriteLine("İsminiz : " + name + " " + surname);
+
+    
+    
+    }
+}
