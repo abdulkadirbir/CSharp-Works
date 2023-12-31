2a434345aca1e5aaf91575a04899d7024e3d3f4e
Abdulkadir Bir <77737235+abdulkadirbir@users.noreply.github.com>
2023-12-31 11:10:18 +0300

    Add files via upload

---
+++ 13-Metot_Overloading/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _13_Metot_Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+            string sayi = "999";
+
+            bool sonuc = int.TryParse(sayi, out int outSayi); // true-false döner 
+            if (sonuc)
+            {
+                Console.WriteLine("Parse işlemi başarılı");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+            Metotlar obje = new Metotlar();
+            obje.Topla(10, 5, out int toplamSonucu); // out ifadeli metotta sonuc doğrudan yazdırılıyor.Normal şekilde olmaz. Out ile direkt.
+            Console.WriteLine(toplamSonucu);
+
+            // Metot Overloading
+            int ifade = 999;
+            obje.EkranaYazdir(ifade);
+            obje.EkranaYazdir("String ifade");
+            // Aynı isimli metotlar görüldüğü gibi tekrar kullanılabildi. Metotlar class ında birden fazla ekrana yazdır var.
+            //Metot imzası yöntemiyle gerçekleşiyor. => metotAdı + parametre sayısı + parametre Üçünden biri farklı olsa yetiyor.
+
+        }
+    }
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri) // public oldu çünkü farklı class tan erişim olacak. Default u private tır
+        {
+            Console.WriteLine(veri);
+        }
+        public int ArttirTopla(ref int deger1, ref int deger2) // referans verildiği için değer kopyalanmadan direkt bellekteki yerine gider ve değiştirir.
+        {
+            deger1++;
+            deger2++;
+            return deger1 + deger2;
+        }
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+    }
+}
---
+++ 14-Recursive_Extension_Metotlar/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _14_Recursive_Extension_Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Recursive metotlar
+            static int factor(int x) // Bu fonksiyon recursive olarak tasarlanmıştır. Böylece sürekli kendisini yeniler.
+                // Fonksiyonların çalışma prensibine  
+            {
+                if (x == 1)
+                {
+                    return x;
+                }
+                return factor(x - 1) * x;
+            }
+            Console.WriteLine(factor(10));
+
+            //Extension metotlar
+            // Extension metotlar muhakkak static class static metot olmak zorunda
+            //Parametre olarak muhakkak this ifadesi kullanılır.
+            //Sonra da hazır kütüphane gibi istediğin yerde kullanılır.
+            int ifade = 10;
+            Console.WriteLine(ifade.isEvenNumber()); //Extension metotlar normal kütüphaneler gibi "." ile çağrılabilir. Bu metotlar özeldir.
+
+            string isim = "Abdulkadir bir";
+            Console.WriteLine(isim.CheckSpaces());
+
+  
+        }
+    }
+
+    public static class Extension
+    {
+        public static bool CheckSpaces(this string param)
+        {
+            return param.Contains(" "); 
+        }
+        public static bool isEvenNumber(this int num)
+        {
+            return num % 2 == 0;
+        }
+
+
+
+    }
+}
