e832778b100a8d821ce9b28210731ec600e5a921
Abdulkadir Bir <77737235+abdulkadirbir@users.noreply.github.com>
2023-12-31 11:06:31 +0300

    Add files via upload

---
+++ 06-If_Else_Ternary If/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _06_If_Else_Ternary_If
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time <= 12)
+                Console.WriteLine("Günaydınlaar!!");
+            else if (time > 12 && time <= 17)
+                Console.WriteLine("İyi Günlerrrr!!");
+            else if (time > 17 && time <= 22)
+                Console.WriteLine("İyi Akşamlar!!!");
+            else
+                Console.WriteLine("İyi Gecelerrr!!");
+            // Ternary ifade için aşağıda örnek incelenebilir. Koşul ? sonuç : koşul sağlanmazsa sonuç
+            Console.WriteLine(time >= 6 && time <= 12 ? "Günaydın" : time > 12 && time <= 17 ? "İyi günler" : time > 17 && time <= 22 ? "İyi akşamlar" : "İyi geceler");
+            string sonuc = time >= 6 && time <= 12 ? "Günaydın" : time > 12 && time <= 17 ? "İyi günler" : time > 17 && time <= 22 ? "İyi akşamlar" : "İyi geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
